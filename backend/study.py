3cc2eefdce51a5f3d200ab6fd824be2527000b06
Logan Ravinuthala <loganravin4@gmail.com>
2024-03-31 12:21:16 -0400

    backend no work on windows

--- backend/study.py
+++ backend/study.py
@@ -13,6 +13,7 @@ import random
 import re
 import time
 from copy import copy as duplicate
+import os
 
 load_dotenv()
 app = Flask(__name__)
@@ -402,7 +403,8 @@ def generate_crossword():
 def main(pdf_file):
     openai.api_key = OPENAI_API_KEY
     # Take user input for notes
-    notes = extract_text(pdf_file)
+    pdf_file.save(os.path.join('files', pdf_file.filename))
+    notes = extract_text((os.path.join('files', pdf_file.filename)))
     
     # Construct a prompt asking for main points and a summary
     prompt = f"Please generate main points and a summary based on the following study notes, format it as a keyphrase : summary, each keyphrase and summary pair is seperated by a period, pretend you are a crossword maker and you are creating answers to the crossword and their corresponding clues:\n{notes}"
