f51906c9d45a5dcb590932a783c827456bd02ac0
Logan Ravinuthala <loganravin4@gmail.com>
2024-03-31 11:40:33 -0400

    revert to last night

--- crossnotes/app/Student/page.tsx
+++ crossnotes/app/Student/page.tsx
@@ -1,15 +1,52 @@
+'use client'
+import { useEffect, useState } from 'react';
 import Link from "next/link";
 import Navbar from "../../components/navbar";
 
 function Student(){
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen">
-          <Navbar />
-          <div className="mt-20 text-center items-center">
-            <h2 className="text-2xl">Welcome Student!</h2>
-          </div>
-        </div>
-      );
+  const [crossword, setCrossword] = useState(null);
+  const [selectedFile, setSelectedFile] = useState<File | undefined>();
+
+  const generateCrossword = async (pdfFile: File) => {
+    const formData = new FormData();
+    formData.append('pdf', pdfFile);
+
+    const response = await fetch('http://localhost:5000/generate_crossword', {
+      method: 'POST',
+      body: formData,
+    });
+    
+    if (!response.ok) {
+      console.log('Network response was not ok');
+      //throw new Error('Network response was not ok');
+    }
+
+    const crossword = await response.json();
+    return crossword;
+  };
+
+  useEffect(() => {
+    if (selectedFile) {
+      generateCrossword(selectedFile).then(setCrossword);
+    }
+  }, [selectedFile]);
+
+  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      setSelectedFile(event.target.files[0]);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <Navbar />
+      <div className="mt-20 text-center items-center">
+        <h2 className="text-2xl">Welcome Student!</h2>
+        <input type="file" accept=".pdf" onChange={handleFileUpload} />
+        {crossword && <p>{JSON.stringify(crossword)}</p>}
+      </div>
+    </div>
+  );
 }
 
 export default Student;
